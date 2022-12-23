7fe18a98ea703c12b83971323e52511f32b598c2
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-23 23:38:02 +0200

    added the first request

---
+++ src/tmdbAPI.js
@@ -0,0 +1,14 @@
+// https://api.themoviedb.org/3/movie/550?api_key=7770a554235a470dd8487676c4d97407
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = '7770a554235a470dd8487676c4d97407';
+
+export const getTrending = async () => {
+  const response = await axios.get(`trending/movie/week?`, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return response.data;
+};
