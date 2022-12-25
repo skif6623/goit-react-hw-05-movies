9a175e6aa15f6acef793604e2bcf18f40f249671
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-25 14:27:27 +0200

    created Movie component markup

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,17 +1,45 @@
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
+import { Search } from 'components/Search/Search';
+import { useEffect, useState } from 'react';
+import { getSearchMovie } from 'servise/tmdbAPI';
+import { SearchList } from 'components/SearchList/SearchList';
 
 export const Movies = () => {
+  const [films, setFilms] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
 
-  const handleSubmit = query => {
-    const queryParams = query !== '' ? { query } : {};
-    setSearchParams(queryParams);
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    const fetchSearchMovie = async query => {
+      const movies = await getSearchMovie(query);
+      setFilms(movies.results);
+    };
+
+    fetchSearchMovie(query);
+  }, [query, films]);
+
+  const setQuery = value => {
+    setSearchParams(value === '' ? {} : { query: value });
   };
 
   return (
-    <>
-      <input type="text" onChange={() => handleSubmit()} />
-      <button type="submit">Search</button>
-    </>
+    <main>
+      <Search onSet={setQuery} />
+      {films && <SearchList films={films} />}
+    </main>
   );
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const movieSearch = searchParams.get('querry') ?? '';
+  // const handleSubmit = query => {
+  //   const queryParams = query !== '' ? { query } : {};
+  //   setSearchParams(queryParams);
+  // };
+  // return (
+  //   <>
+  //     <Search value={movieSearch} onChange={handleSubmit} />
+  //   </>
+  // );
 };
